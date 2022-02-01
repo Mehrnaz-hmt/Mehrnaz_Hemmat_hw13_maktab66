5e573e412642f0debf2e5a256c3a3300a831573f
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-02-02 02:05:33 +0330

    add clone / delete btn to q-1

--- Question 1/srcipt-1.js
+++ Question 1/srcipt-1.js
@@ -6,30 +6,41 @@ let delBtn = $("#del-btn");
 let myInput1 = $("#my-input-1");
 let radioBox = $("td").children();
 
+//when I click on add btn :
+$(".add-row").on('click', addRow);
+function addRow() {
+    $("#my-tr").clone().insertAfter("#my-tr");
+};
 
-$(document).ready(function () {
-  $(".add-row").click(function () {
-    table.append(
-      "<tr><td>" +
-        delBtn +
-        "</td><td>" +
-        myInput1 +
-        "</td><td>" +
-        myInput1 +
-        "</td>" +
-        "<td>" +
-        radioBox +
-        "</td>" +
-        "</tr>"
-    );
-  });
-});
-
-//when I click on Delete btn
+//when I click on Delete btn:
 $(document).ready(function() {
     $(".table").on("click", "#del-btn", function() {
         $(this).closest("tr").remove();
      }); 
-
 })
 
+//when I click on clone btn:
+var r = $(this).closest('tr').clone();
+$(document).on('click',"#clone-btn", function(e)
+{
+  var r = $(this).closest('tr').clone();
+  $(this).closest('tr').after(r);
+});
+
+// $(document).ready(function () {
+//   $(".add-row").click(function () {
+//     table.append(
+//       "<tr><td>" +
+//         delBtn +
+//         "</td><td>" +
+//         myInput1 +
+//         "</td><td>" +
+//         myInput1 +
+//         "</td>" +
+//         "<td>" +
+//         radioBox +
+//         "</td>" +
+//         "</tr>"
+//     );
+//   });
+// });
