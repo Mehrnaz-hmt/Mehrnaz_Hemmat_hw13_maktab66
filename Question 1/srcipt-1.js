a2944ba202361ba738b5d72b057239b2e53d4a8d
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-02-06 13:49:14 +0330

    all rows numbers -- mood

--- Question 1/srcipt-1.js
+++ Question 1/srcipt-1.js
@@ -3,7 +3,7 @@ let table = tbody.length ? tbody : $("table");
 let tr = $(tbody).children();
 let td = $("#my-td");
 let delBtn = $("#del-btn");
-let myInput1 = $("#my-input-1");
+let myInput1 = $("#my-input-number");
 let radioBox = $("td").children();
 let counter = 0;
 let rowCount = $("table tbody tr").length;
@@ -12,78 +12,86 @@ let rowCount = $("table tbody tr").length;
  $(".add-row").on('click', addRow);
  function addRow() {
    counter++;
-     $("tbody").append(`
-     <tr id="my-tr">
-     <td id="my-td" scope="row">
-       <button
-         style="
-           background-color: rgb(250, 11, 11);
-           border: none;
-           color: white;
-           padding: 4px 8px;
-           font-size: 16px;
-           cursor: pointer;
-         "
-         id="del-btn"
-         class="btn"
-         ><i class="fa fa-trash"></i> Delete</button
-       >
-       <button
-         style="
-           background-color: rgb(81, 116, 212);
-           border: none;
-           color: white;
-           padding: 4px 8px;
-           font-size: 16px;
-           cursor: pointer;
-         "
-         id="clone-btn"
-         class="btn"
-         ><i class="fa fa-clone"></i> Clone</button
-       >
+   let thisRow = `
+   <tr id="my-tr">
+   <td id="my-td" scope="row">
+     <button
+       style="
+         background-color: rgb(250, 11, 11);
+         border: none;
+         color: white;
+         padding: 4px 8px;
+         font-size: 16px;
+         cursor: pointer;
+       "
+       id="del-btn"
+       class="btn"
+       ><i class="fa fa-trash"></i> Delete</button
+     >
+     <button
+       style="
+         background-color: rgb(81, 116, 212);
+         border: none;
+         color: white;
+         padding: 4px 8px;
+         font-size: 16px;
+         cursor: pointer;
+       "
+       id="clone-btn"
+       class="btn"
+       ><i class="fa fa-clone"></i> Clone</button
+     >
+   </td>
+   <div class="col-3">
+     <td><input type="text" id="my-input-number" /></td>
+     <td><input type="text" /></td>
+     <td>
+       <div class="form-check form-check-inline">
+         <input
+           class="form-check-input"
+           type="radio"
+           name="inlineRadioOptions${counter}"
+           id="inlineRadio1"
+           value="option1"
+         />
+         <label class="form-check-label" for="inlineRadio1">New</label>
+       </div>
+       <div class="form-check form-check-inline">
+         <input
+           class="form-check-input"
+           type="radio"
+           name="inlineRadioOptions${counter}"
+           id="inlineRadio2"
+           value="option2"
+         />
+         <label class="form-check-label" for="inlineRadio2"
+           >In Progresse</label
+         >
+       </div>
+       <div class="form-check form-check-inline">
+         <input
+           class="form-check-input"
+           type="radio"
+           name="inlineRadioOptions${counter}"
+           id="inlineRadio3"
+           value="option3"
+         />
+         <label class="form-check-label" for="inlineRadio3"
+           >Confirmed</label
+         >
+       </div>
      </td>
-     <div class="col-3">
-       <td><input type="text" id="my-input-1" /></td>
-       <td><input type="text" /></td>
-       <td>
-         <div class="form-check form-check-inline">
-           <input
-             class="form-check-input"
-             type="radio"
-             name="inlineRadioOptions${counter}"
-             id="inlineRadio1"
-             value="option1"
-           />
-           <label class="form-check-label" for="inlineRadio1">New</label>
-         </div>
-         <div class="form-check form-check-inline">
-           <input
-             class="form-check-input"
-             type="radio"
-             name="inlineRadioOptions${counter}"
-             id="inlineRadio2"
-             value="option2"
-           />
-           <label class="form-check-label" for="inlineRadio2"
-             >In Progresse</label
-           >
-         </div>
-         <div class="form-check form-check-inline">
-           <input
-             class="form-check-input"
-             type="radio"
-             name="inlineRadioOptions${counter}"
-             id="inlineRadio3"
-             value="option3"
-           />
-           <label class="form-check-label" for="inlineRadio3"
-             >Confirmed</label
-           >
-         </div>
-       </td>
-     </div>
-   </tr>
-     `);
+   </div>
+ </tr>
+   `
+     $("tbody").append(thisRow);
+    constradioBoxCont =  $(".radio-box-cont");
+  //
+ if (radioBoxCont.val() == "confirmed"){
+  $("#my-input-number").prop('disabled', true);
+  $("#my-input-type").prop('disabled', true);
+ }
+
  };
 
 
@@ -91,10 +99,12 @@ let rowCount = $("table tbody tr").length;
 $(document).ready(function() {
     $(".table").on("click", "#del-btn", function() {
         $(this).closest("tr").remove();
+
+        var rowCount = $("table tr").length-1;
+      $(".all-rows").html(`All rows number: ${rowCount--}`);
      }); 
      
-    var allRowsValue= $(".all-rows").val();
-    $("allRowsValue").html(`All rows number: ${rowCount--}`);
+   
   
      
 })
@@ -108,7 +118,7 @@ $(document).on('click',"#clone-btn", function(e)
 });
 
 
-//All rows number:
+//All rows numbers:
 $(document).ready(function(){
   $('.add-row').click(function(){
       var rowCount = $("table tr").length-1;
@@ -118,35 +128,20 @@ $(document).ready(function(){
 });
 
 //radioBox in confirmed mood
-$('.radio-box-cont').on('click', function() {
-  if($('input[value=Confirmed]:checked')) {
-    $("#my-input-1").prop('disabled', true);
-    $("#my-input-2").prop('disabled', true);
-  }
-});
-
-//Not confirmed row btn:
-
-// let ncrownumber = 0;
-//   if($('input[!value==Confirmed]:checked')) {
-//     $(".nc-rows-Number").html(`Not comfired rows number: ${ncrownumber++}`)
+// $('.radio-box-cont').on('click', function() {
+//   if($('input[value=Confirmed]:checked')) {
+//     $("#my-input-number").prop('disabled', true);
+//     $("#my-input-type").prop('disabled', true);
 //   }
-
-// $('.add-row').click(function(){
-//   var rowCount = $("table tr").length-1;
-//  let nConfirmedBtn =  $('input[name="radio-box-cont"]:checked').val();
-//  if(nConfirmedBtn=="confirmed"){
-
-//  }
 // });
 
-//test 
-$("table").on("click", ".new", function (e) {
-  let check = $(this).closest(".thisRow");
-  let inputs = $(":text");
-  if (e.target.checked) {
-    check.find(inputs)[0].disabled = false;
-    check.find(inputs)[1].disabled = false;
+
+
+$('.radio-box-cont').on('click', function(e) {
+  if($(e.target.val == "confirmed")) {
+    $("#my-input-number").prop('disabled', true);
+    $("#my-input-type").prop('disabled', true);
   }
-  checkConfrimed()
 });
+
+
